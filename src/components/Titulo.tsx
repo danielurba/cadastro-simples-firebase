a245532ce15b329aa7c213a4ca08542cc46f2e14
Daniel urbaneki <daniel.urbaneki.uniguacu@gmail.com>
2024-02-22 09:57:35 -0300

    Conclusão de projeto da cod3r.

--- src/components/Titulo.tsx
+++ src/components/Titulo.tsx
@@ -1,4 +1,8 @@
-export default function Titulo(props) {
+interface TituloProps {
+    children: any
+}
+
+export default function Titulo(props: TituloProps) {
     return (
         <div className="flex flex-col justify-center">
             <h1 className="px-5 py-2 text-2xl">
